5dbaf3a12a9d917feb1b97ca838e573ce9e52c85
Mikhail Voronin <mikhail.voronin@docusign.com>
2017-09-11 18:56:15 -0400

    second try

--- src/main/java/com/dreamforce17/herokucamel/MainApp.java
+++ src/main/java/com/dreamforce17/herokucamel/MainApp.java
@@ -1,14 +1,26 @@
 package com.dreamforce17.herokucamel;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.ProducerTemplate;
+import org.apache.camel.Exchange;
+import org.apache.camel.Predicate;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.salesforce.SalesforceComponent;
-import org.apache.camel.component.weather.WeatherComponent;
+import org.apache.camel.component.salesforce.api.dto.CreateSObjectResult;
+import org.apache.camel.component.telegram.model.IncomingMessage;
+import org.apache.camel.component.telegram.model.OutgoingTextMessage;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.salesforce.dto.Case;
+import org.apache.camel.salesforce.dto.Case_PriorityEnum;
+import org.apache.camel.salesforce.dto.Contact;
+import org.apache.camel.salesforce.dto.QueryRecordsContact;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MainApp {
 	public static void main(String args[]) throws Exception {
+		
 		CamelContext context = new DefaultCamelContext();
 		
 		SalesforceComponent salesforce = new SalesforceComponent();
@@ -23,18 +35,162 @@ public class MainApp {
 		
 		context.addRoutes(new RouteBuilder() {
 			public void configure() {
+				
+				/*
+				 * Route 2: update Telegram user with Salesforce updates
+				 * */
+				from("salesforce:ChangeCaseTopic?notifyForFields=SELECT&NotifyForOperationCreate=false&sObjectName=Case&updateTopic=true&sObjectQuery=SELECT Id, Status, Telegram_Chat_Id__c FROM Case WHERE Telegram_Chat_Id__c != NULL")
+					.log("Case ${body.getId} update with status \'${body.getStatus}\', chatId = ${body.getTelegram_Chat_Id__c}")
+					.process(new Processor() {
+						@Override
+						public void process(Exchange exchange) throws Exception {
+							// TODO Auto-generated method stub
+							Case updatedCase = exchange.getIn().getBody(Case.class);
+							OutgoingTextMessage messageOut = new OutgoingTextMessage();
+							messageOut.setText("Case id " + updatedCase.getId() + "is now \"" + updatedCase.getStatus() + "\"");
+							messageOut.setChatId(updatedCase.getTelegram_Chat_Id__c());
+							exchange.getIn().setBody(messageOut);
+						}
+					})
+					.to("telegram:bots/359951231:AAEr0BMcYe0TJIu2tFQ8xnAtAI8QHM8TBfM")
+				;
+				
+		
+				/*
+				 * Route 1: convert incoming Telegram chat message to Salesforce case
+				 * */
+				
+				/*
+				 * Take incoming message from telegram
+				 * */
 				from("telegram:bots/359951231:AAEr0BMcYe0TJIu2tFQ8xnAtAI8QHM8TBfM")
-					.process(new TelegrammProcessor())
-					.to("telegram:bots/359951231:AAEr0BMcYe0TJIu2tFQ8xnAtAI8QHM8TBfM");
-				//from("direct:toSalesforce")
-				//	.to("salesforce:limits")
-				//	.process(new SalesforceProcessor());
+					
+					/*
+					 * Save text of the message to message header
+					 * */
+					.process(new Processor() {
+						@Override
+						public void process(Exchange exchange) throws Exception {
+							IncomingMessage telegramMessage = exchange.getIn().getBody(IncomingMessage.class);
+							String message = telegramMessage.getText();
+							exchange.getIn().setHeader("TelegramRequest", message);
+						}
+					})
+					
+					/*
+					 * Alternative way to save a message as 
+					 * */
+					//.setHeader("TelegramRequest", bodyAs(String.class))
+					
+					/*
+					 * Lookup contact in Salesforce which FirstName and LastName equals Telegram message () 
+					 */
+					.toD("salesforce:query?sObjectQuery=SELECT Id, MailingCity FROM Contact WHERE FirstName=\'${body.from.firstName}\' AND LastName=\'${body.from.lastName}\'&sObjectClass=org.apache.camel.salesforce.dto.QueryRecordsContact")
+					
+					/*
+					 * Filter out (stop message processing) if no Contact found in Salesforce
+					 * */
+					.filter(new Predicate() {
+						@Override
+						public boolean matches(Exchange exchange) {
+							QueryRecordsContact foundContacts = exchange.getIn().getBody(QueryRecordsContact.class);
+							boolean filterResult = foundContacts.getRecords().size() == 1;
+							exchange.getIn().setHeader("Contact", foundContacts.getRecords().get(0));
+							return filterResult;
+						}
+					})
+					
+					/*
+					 * Alternative way to filter message (use lambda) 
+					 * */
+					//.filter(e -> e.getIn().getBody(QueryRecordsContact.class).getRecords().size() == 1)
+					
+					/*
+					 * If found Contact has zip code in the address - find weather for this address
+					 */
+					.choice()
+					
+						.when(new Predicate() {
+							@Override
+							public boolean matches(Exchange exchange) {
+								QueryRecordsContact foundContacts = exchange.getIn().getBody(QueryRecordsContact.class);
+								Contact contact = foundContacts.getRecords().get(0);
+								return contact.getMailingCity() != null;
+							}	
+						})
+						
+						/*
+						 * Request weather for the Contact location 
+						 * */
+						.toD("weather:get?location=${body.records[0].getMailingCity}&appid=b1a7b97da054ad4ca0730463120ce1ba")
+						
+						
+						/*
+						 * Saves temperature for the location
+						 * */
+						.process(new Processor() {
+							@Override
+							public void process(Exchange exchange) throws Exception {
+								ObjectMapper objectMapper = new ObjectMapper();
+								JsonNode weatherInfo = objectMapper.readTree(exchange.getIn().getBody(String.class));
+								Double temperature = weatherInfo.get("main").get("temp").asDouble();
+								exchange.getIn().setHeader("Temperature", temperature);
+							}				
+						})
+					
+					/*
+					 * End if
+					 */
+					.end()
+					
+					/*
+					 * Prepares the case to be created in Salesforce
+					 * */
+					.process(new Processor() {
+						@Override
+						public void process(Exchange exchange) throws Exception {
+							Contact contact = exchange.getIn().getHeader("Contact", Contact.class);
+							String chatId = exchange.getIn().getHeader("CamelTelegramChatId", String.class);
+							Double temperature = exchange.getIn().getHeader("Temperature", Double.class);
+							boolean highTemperature = (temperature != null) && (temperature.doubleValue() > 300);
+							
+							Case newCase = new Case();
+							newCase.setContactId(contact.getId());
+							newCase.setTelegram_Chat_Id__c(chatId);
+							newCase.setSubject((String)exchange.getIn().getHeader("TelegramRequest"));
+							newCase.setPriority(highTemperature ? Case_PriorityEnum.HIGH : Case_PriorityEnum.LOW);
+							exchange.getIn().setBody(newCase);
+						}
+					})
+					
+					/*
+					 * Create new case in Salesforce 
+					 */
+					.to("salesforce:createSObject?sObjectClass=org.apache.camel.salesforce.dto.Case")
+					
+					/*
+					 * Prepares final response to Telegram Chat
+					 * */
+					.process(new Processor() {
+						@Override
+						public void process(Exchange exchange) throws Exception {
+							CreateSObjectResult newCase = exchange.getIn().getBody(CreateSObjectResult.class);
+							OutgoingTextMessage messageOut = new OutgoingTextMessage();
+							messageOut.setText("Created case id " + newCase.getId());
+							exchange.getIn().setBody(messageOut);
+						}
+					})
+					
+					/*
+					 * Writes message back to user
+					 * */
+					.to("telegram:bots/359951231:AAEr0BMcYe0TJIu2tFQ8xnAtAI8QHM8TBfM")
+				;
 			}
 		});
 		context.start();
 		//ProducerTemplate producerTemplate = context.createProducerTemplate();
 		//producerTemplate.sendBody("direct:toSalesforce", "Start route");
-		Thread.sleep(100000);
-		context.stop();
+		Thread.sleep(10000000);
 	}
 }
--- src/main/java/com/dreamforce17/herokucamel/SalesforceProcessor.java
+++ src/main/java/com/dreamforce17/herokucamel/SalesforceProcessor.java
@@ -1,15 +1,30 @@
 package com.dreamforce17.herokucamel;
 
+import org.apache.camel.Body;
 import org.apache.camel.Exchange;
-import org.apache.camel.Message;
+import org.apache.camel.Header;
 import org.apache.camel.Processor;
+import org.apache.camel.salesforce.dto.Contact;
+import org.apache.camel.salesforce.dto.QueryRecordsContact;
+import org.apache.camel.salesforce.dto.Case;
 
-public class SalesforceProcessor implements Processor {
+public class SalesforceProcessor /*implements Processor*/ {
 
+	/*
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		Message msg = exchange.getIn();
-		Object sf = msg.getBody();
-		System.out.println(sf);
+		Contact contact = exchange.getIn().getBody(QueryRecordsContact.class).getRecords().get(0);
+		Case newCase = new Case();
+		newCase.setContactId(contact.getId());
+		newCase.setSubject((String)exchange.getIn().getHeader("TelegramRequest"));
+		exchange.getIn().setBody(newCase);
+	}
+	*/
+	
+	public Case process(@Body QueryRecordsContact contacts, @Header("TelegramRequest") String request) {
+		Case newCase = new Case();
+		newCase.setContactId(contacts.getRecords().get(0).getId());
+		newCase.setSubject(request);
+		return newCase;
 	}
 }
